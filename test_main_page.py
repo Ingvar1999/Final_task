760ed29954b16416696a59774127b1c6971f2e2a
Ingvar1999 <yiv1999@mail.ru>
2024-07-16 20:40:02 +0300

    Добавление файлов base_page.py и main_page.py из Курс 2, Lesson 4.2, step 4

--- test_main_page.py
+++ test_main_page.py
@@ -1,10 +1,22 @@
+from .pages.main_page import MainPage
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    time.sleep(10)
+    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+    page.open()                      # открываем страницу
+    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
+
+# Версия от 16.07.2024 г. 20:34
+
+def go_to_login_page(browser):
     login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
     login_link.click()
+
+def test_guest_can_go_to_login_page(browser): 
+    link = "http://selenium1py.pythonanywhere.com/"
+    browser.get(link) 
+    time.sleep(10)
+    go_to_login_page(browser) 
