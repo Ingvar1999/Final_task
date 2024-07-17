8cb083b08ae920d57be69f99cc5a47d433ee3347
Ingvar1999 <yiv1999@mail.ru>
2024-07-17 12:51:08 +0300

    Lesson 4.2, step 9 Переходы между страницами

--- test_main_page.py
+++ test_main_page.py
@@ -5,10 +5,11 @@ from selenium.webdriver.common.by import By
 import time
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
-    page.open()                      # открываем страницу
-    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
+    link = "http://selenium1py.pythonanywhere.com"
+    page = MainPage(browser, link)
+    page.open()
+    login_page = page.go_to_login_page()
+    login_page.should_be_login_page()
 
 # Версия от 17.07.2024 г. 08:34
 
